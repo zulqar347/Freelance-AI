3cde482ef00eb50187728369bf1d43792b349fce
zulqar347 <zulqurnainghufran407@gmail.com>
2026-07-09 01:45:06 -0700

    updated proxy.ts to include all the routes that need to be protected

--- proxy.ts
+++ proxy.ts
@@ -3,8 +3,25 @@ import authConfig from "./auth.config";
 
 const { auth } = NextAuth(authConfig);
 
-export const proxy = auth((req) => {});
+export const proxy = auth((req) => {
+  const isLoggedIn = !!req.auth;
+
+  if (!isLoggedIn) {
+    return Response.redirect(new URL("/auth", req.nextUrl));
+  }
+});
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: [
+    "/dashboard/:path*",
+    "/billing/:path*",
+    "/settings/:path*",
+    "/career-profile/:path*",
+    "/onboarding/:path*",
+    "/portfolio/:path*",
+    "/cover-letter-generator/:path*",
+    "/profile-generator/:path*",
+    "/proposal-generator/:path*",
+    "/resume-generator/:path*",
+  ],
 };
